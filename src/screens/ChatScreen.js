72e83bc930440dff8b57977b030af894437ff9ea
praveenbhat73 <bhatpraveen721@gmail.com>
2022-11-07 22:28:01 +0530

    styled input

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,4 +1,4 @@
-import { View,Text,ImageBackground,StyleSheet,FlatList } from "react-native";
+import { View,Text,ImageBackground,StyleSheet,FlatList,KeyboardAvoidingView } from "react-native";
 import Message from "../components/Message";
 
 import messages from '../../assets/data/messages.json'
@@ -6,6 +6,7 @@ import bg from '../../assets/images/BG.png'
 import InputBox from "../components/InputBox";
 // native special component to display image in background and remaining in top of it 
 // its ImageBackground and remaining acts as child component and stays above it 
+// const image = { uri: "https://i.pinimg.com/736x/85/04/30/850430a750fb80c1ebaa5e740fc7cbd6.jpg" };
 const ChatScreen=()=>{
     return (
 <>
@@ -13,7 +14,7 @@ const ChatScreen=()=>{
     behavior={Platform.OS === "ios" ? "padding" : "height"}
     style={styles.bg}
   >
-<ImageBackground source={bg} style={styles.bg}>
+<ImageBackground  source={bg} style={styles.bg}>
     <FlatList
     data={messages}
     renderItem={({item})=> <Message message={item}/>}
@@ -31,6 +32,7 @@ const ChatScreen=()=>{
 const styles=StyleSheet.create({
     bg:{
         flex:1,
+       
         
     },
     list:{
